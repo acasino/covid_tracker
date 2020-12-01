2a1f2ab7dd10a3146fa7b76f0219e9f30e380976
Andrew <andrew.casin@gmail.com>
2020-12-01 01:04:32 -0600

    Added attr_accessor and initialize to Coviddata

--- lib/coviddata.rb
+++ lib/coviddata.rb
@@ -1,2 +1,19 @@
-class Coviddata
+class Covidtracker::Coviddata
+
+    attr_accessor :date, :state, :states, :positive, :negative, :pending, :hospitalized, :recovered, :lastModified, :death, :deathIncrease, :hospitalizedIncrease, :negativeIncrease, :positiveIncrease, :totalTestResultsIncrease, :dataQualityGrade
+
+    @@all = []
+
+    def initialize(data_hash)
+        data_hash.each do |key, value|
+            self.class.attr_accessor key
+            self.send("#{key}=", value)
+        end
+        @@all << self
+        binding.pry
+    end
+
+
 end
+
+# Covidtracker::Coviddata.new.initialize
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,16 +4,19 @@ class Covidtracker::Scraper
 
     #initialize url
     def initialize
+        #URL for all states current data
         @url = "https://api.covidtracking.com/v1/states/current.json"
     end
 
-    #get current data, all states
+    #get all states current data
     def get_all_state_current
         uri = URI(@url)
         response = Net::HTTP.get(uri) #return response as string
         data = JSON.parse(response) #parse JSON, return array
-        binding.pry
+        Covidtracker::Coviddata.new(data) #initialize new instance of all state current data
     end
+
+
 
     
 
