7e6f61124480018f3bb9c783c4667235de2cb83a
Andrew <andrew.casin@gmail.com>
2020-12-01 00:15:34 -0600

    Scraper file updates

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,1 +1,25 @@
 require_relative "../config/environment"
+#Scraper file to get data
+class Covidtracker::Scraper
+
+    #initialize url
+    def initialize
+        @url = "https://api.covidtracking.com/v1/states/current.json"
+    end
+
+    #get current data, all states
+    def get_all_state_current
+        uri = URI(@url)
+        response = Net::HTTP.get(uri)
+        data = JSON.parse(response)
+
+    end
+
+
+    
+
+
+
+end
+
+
